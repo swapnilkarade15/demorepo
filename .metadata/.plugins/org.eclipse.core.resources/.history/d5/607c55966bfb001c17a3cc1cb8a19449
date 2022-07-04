a7556b282cf72144a8b8ede1635293048db7fb68
Swapnil Karade <swapnilkarade15>
2022-07-04 13:15:40 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d5/607c55966bfb001c17a3cc1cb8a19449
@@ -0,0 +1,10 @@
+package Test;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DemoGit/src/Test/Class2.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=6,c;
+
+		c=a*b;
+		
+		System.out.println("addition is  = "+c);
+		
+	}
+
+}
