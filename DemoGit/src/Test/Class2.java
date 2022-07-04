793f0c56d92933cfb4afd04e9fa2ca19e701f45b
Swapnil Karade <swapnilkarade15>
2022-07-04 15:58:15 +0530

    modification done by  tester in test branch

--- DemoGit/src/Test/Class2.java
+++ DemoGit/src/Test/Class2.java
@@ -4,11 +4,17 @@ public class Class2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=5,b=6,c;
+		int a=5,b=6,c,d,e=5,f=5;
 
 		c=a*b;
 		
 		System.out.println("addition is  = "+c);
+		
+		
+		d=e/f;
+		
+		System.out.println("division   = "+d);
+		
 		
 	}
 
